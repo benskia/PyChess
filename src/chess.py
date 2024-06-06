24d697b9d94c04cee782a1ca8a956493bc269d48
benskia <benski.abraham@gmail.com>
2024-06-06 18:04:43 -0500

    add simple run_game

--- src/chess.py
+++ src/chess.py
@@ -12,10 +12,18 @@ from command_validators import validate_command
 class Game:
 
     def __init__(self) -> None:
-        self.is_white: bool = True
-        self.board = Board()
+        self._input_prompt: str = "Input command in algebraic notation (ex: Nb1e3): "
+        self._is_white: bool = True
+        self._board = Board()
 
-    def parse_command(self, command: str) -> Command | None:
+    def run_game(self) -> None:
+        print(self._board)
+        cmd: Command | None = None
+        while not cmd:
+            cmd = self.__parse_command(input(self._input_prompt))
+        print("Successfully created command!")
+
+    def __parse_command(self, command: str) -> Command | None:
         if not validate_command(command):
             return None
         x1: int = ord(command[1]) - 97
--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from chess import Game
 
 def main() -> None:
     g = Game()
-    print(g.board)
-    g.parse_command("ooWeee")
-    g.parse_command("Wowwo")
-    g.parse_command("Nb1e3")
+    g.run_game()
 
 
 if __name__ == "__main__":
