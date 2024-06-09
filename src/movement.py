85836bd082b9a5d51b1cf3b5ee13650ee2078812
benskia <benski.abraham@gmail.com>
2024-06-08 19:19:45 -0500

    brain no worky

--- src/movement.py
+++ src/movement.py
@@ -8,6 +8,14 @@ def validate_move(cmd: Command, board: list[list[Piece | None]]) -> bool:
         return False
     print(source._movement_patterns)
     # TODO: Need some logic for determining valid movements. Perhaps comparing
-    # the difference between origin and destination x:y values.
+    # the (origin x:y - destination x:y) and piece movement patterns.
+    x_diff: int = abs(cmd._x1 - cmd._x2)
+    y_diff: int = abs(cmd._y1 - cmd._y2)
     return True
 
+
+def validate_pattern(cmd_pattern: tuple[int, int], pc: Piece) -> bool:
+    if cmd_pattern not in pc._movement_patterns:
+        print(f"{cmd_pattern} not found in {pc} movement patterns")
+        return False
+    return True
