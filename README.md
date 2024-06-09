1b476692748ff1cee80f1b4144c66054ff23212f
benskia <benski.abraham@gmail.com>
2024-06-09 08:26:05 -0500

    add tests

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ algebraic notation. For example, "Nb1c3" moves white's Knight at b1 to c3.
 
 ## Roadmap
 
-* If first move, Pawns can move two spaces.
-* If attacking, Pawns can move diagonally.
+* Pawns can move two spaces as first movement.
+* Pawns can capture on diagonal and only on diagonal.
 * Check(mate) logic
 * Castling
--- src/test.py
+++ src/test.py
@@ -2,6 +2,9 @@ import unittest
 
 from chess import Game
 from command import Command, validate_command
+from board import Board
+from movement import validate_move
+from piece import *
 
 
 class TestChess(unittest.TestCase):
@@ -9,21 +12,31 @@ class TestChess(unittest.TestCase):
     def setUp(self) -> None:
         self.game = Game()
 
-    # Command Parsing
-
     def test_invalid_cmdstr(self):
         invalid_cmdstr: str = "Nf5"
-        self.assertEqual(validate_command(invalid_cmdstr), False)
+        self.assertFalse(validate_command(invalid_cmdstr))
 
     def test_valid_cmdstr(self):
         valid_cmdstr: str = "Nb1c3"
-        self.assertEqual(validate_command(valid_cmdstr), True)
+        self.assertTrue(validate_command(valid_cmdstr))
 
     def test_parse_command(self):
         valid_cmdstr: str = "Nb1c3"
-        expected_cmd: Command = Command("N", 1, 0, 2, 2)
-        test_cmd: Command = self.game.__parse_command(valid_cmdstr)
-        self.assertEqual(str(test_cmd), str(expected_cmd))
+        test_cmd: Command = self.game._Game__parse_command(valid_cmdstr)
+        valid_cmd: Command = Command("N", 1, 0, 2, 2)
+        self.assertEqual(str(test_cmd), str(valid_cmd))
+
+    def test_invalid_move(self):
+        invalid_cmd: Command = Command("N", 1, 0, 2, 5)
+        self.assertFalse(validate_move(invalid_cmd, self.game._board))
+
+    def test_valid_move(self):
+        valid_cmd: Command = Command("N", 1, 0, 2, 2)
+        expected_board: Board = Board()
+        expected_board._grid[0][1] = None
+        expected_board._grid[2][2] = Knight(True)
+        self.game._Game__execute_command(valid_cmd)
+        self.assertEqual(str(self.game._board), str(expected_board))
 
 
 if __name__ == "__main__":
